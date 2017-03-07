b71a4b753661d6b453bb3dfb19e660d471e42778
Brian Anders <anders.brian@icloud.com>
2017-03-06 20:08:33 -0800

    Initial commit with working extension

---
+++ extension.js
@@ -0,0 +1,26 @@
+const vscode = require('vscode');
+const editor = vscode.window.activeTextEditor;
+
+function duplicateText() {
+
+  if (!editor) {
+    return;
+  }
+
+  editor.edit(builder => {
+    editor.selections.forEach(selection => {
+      const text = editor.document.getText(selection);
+      const position = selection.start;
+
+      console.log(text);
+      builder.insert(position, text);
+    });
+  });
+}
+
+exports.activate = function activate(context) {
+  const registeredCommand = vscode.commands.registerCommand('sublime-duplicate-text.duplicateText', duplicateText);
+  context.subscriptions.push(registeredCommand);
+}
+
+exports.deactivate = function deactivate() { };
